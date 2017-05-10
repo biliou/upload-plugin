4438d7d4bd5241ca1c0b77d87a8cfb1636eeb978
bili.ou <bili.ou@cyphertech.cn>
2017-05-10 13:39:06 +0800

    上传文件(http请求失败)

---
+++ src/components/FileUpload.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>upload file</h1>
+    <!--上传图片-->
+    <div id="wp" class="upload_warpper">
+      <upload></upload>
+    </div>
+  </div>
+</template>
+
+<script>
+import Upload from './extend/upload'
+export default {
+  components: {
+    Upload
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+</style>
